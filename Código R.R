dc674061a435e9af6a60c8628c8b1e93b7348503
Martín <mgallo@mail.austral.edu.ar>
2024-11-04 16:17:51 -0300

    Codigito

--- Código R.R
+++ Código R.R
@@ -1,3 +1,8 @@
 library(tidyverse)
 library(caret)
 ggplot()
+library(usethis)
+use_git_config(user.name = "Martin", user.email = "mgallo@mail.austral.edu.ar
+")
+gitcreds::gitcreds_set()
+usethis::create_github_token()
